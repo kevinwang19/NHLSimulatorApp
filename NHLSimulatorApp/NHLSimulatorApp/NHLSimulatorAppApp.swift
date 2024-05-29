5358a5cd82f624e7bea992ac8d5f670f88eb4027
Kevin Wang <kevin.wang19@yahoo.com>
2024-05-28 23:22:44 -0400

    Set up swift environment

---
+++ NHLSimulatorApp/NHLSimulatorApp/NHLSimulatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NHLSimulatorAppApp.swift
+//  NHLSimulatorApp
+//
+//  Created by Kevin Wang on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct NHLSimulatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
